b2c236a15e17be806e327c682b8eb4210b0ede17
Atak Kan <lava@Ataks-MacBook-Pro.local>
2024-12-10 16:29:34 +0100

    changes are done

--- mmfreelm/layers/hgrn_bit.py
+++ mmfreelm/layers/hgrn_bit.py
@@ -42,20 +42,25 @@ class HGRNBitAttention(nn.Module):
         self.expand_ratio = expand_ratio
         self.input_dim = int(hidden_size * expand_ratio)
         self.head_dim = self.input_dim // self.num_heads
-
+        self.recurrent_state = None
         self.use_short_conv = use_short_conv
         self.conv_size = conv_size
         self.conv_bias = conv_bias
         self.share_conv_kernel = share_conv_kernel
+
+
+        self.rms_norm_custom = torch.nn.RMSNorm(hidden_size)
 
         self.layer_idx = layer_idx
 
         assert mode in ['fused_recurrent'], f"Not suppoerted mode `{mode}`."
         assert self.hidden_size % num_heads == 0, f"hidden size must be divisible by num_heads of {num_heads}"
 
-        self.i_proj = BitLinear(hidden_size, self.input_dim, bias=False)
-        self.f_proj = BitLinear(hidden_size, self.input_dim, bias=False)
-        self.g_proj = BitLinear(hidden_size, self.input_dim, bias=False)
+        self.i_proj = BitLinear(hidden_size, self.input_dim, bias=False)#i
+        self.f_proj = BitLinear(hidden_size, self.input_dim, bias=False)#g
+        self.g_proj = BitLinear(hidden_size, self.input_dim, bias=False)#o
+
+        
 
         if use_short_conv:
             self.conv_size = conv_size
@@ -65,6 +70,7 @@ class HGRNBitAttention(nn.Module):
                 self.q_conv1d = ShortConvolution(self.input_dim, conv_size, activation='silu')
                 self.f_conv1d = ShortConvolution(self.input_dim, conv_size, activation='silu')
                 self.i_conv1d = ShortConvolution(self.input_dim, conv_size, activation='silu')
+                self.g_conv1d = ShortConvolution(self.input_dim, conv_size, activation='silu')
 
         self.g_norm = FusedRMSNormSwishGate(self.input_dim, layernorm_eps)
         self.o_proj = BitLinear(self.input_dim, hidden_size, bias=False)
@@ -94,6 +100,12 @@ class HGRNBitAttention(nn.Module):
         mode = 'fused_recurrent' if hidden_states.shape[1] == 1 else self.mode
 
         last_state = past_key_values[self.layer_idx] if use_cache else None
+        if past_key_values is not None:
+            print("Past Key Values", len(past_key_values))
+        if last_state is not None:
+            print("last_state", last_state.shape)
+       
+
         if self.use_short_conv:
             conv_state = last_state[0] if use_cache else None
             if self.share_conv_kernel:
@@ -101,42 +113,62 @@ class HGRNBitAttention(nn.Module):
                 hidden_states = self.h_conv1d(hidden_states, attention_mask, conv_state)
                 i = self.i_proj(hidden_states)
                 f = self.f_proj(hidden_states)
+                g = self.g_proj(hidden_states)
             else:
                 conv_state_i = last_state[2] if use_cache else None
                 conv_state_f = last_state[1] if use_cache else None
+                conv_state_g = last_state[0] if use_cache else None #WWWHYYYYYYYY
                 i = self.i_conv1d(self.i_proj(hidden_states), attention_mask, conv_state_i)
                 f = self.f_conv1d(self.f_proj(hidden_states), attention_mask, conv_state_f)
+                g = self.g_conv1d(self.g_proj(hidden_states), attention_mask, conv_state_g)
         else:
             i = self.i_proj(hidden_states)
             f = self.f_proj(hidden_states)
+            g = self.g_proj(hidden_states)
 
+      
+        
+        ########################################
         f = f.sigmoid()
         # the lower bound for the first layer is zero
         if lower_bound is not None and self.layer_idx > 0:
             f = lower_bound + (1 - lower_bound) * f
-        i = swiglu(i, 1 - f)
+        ########################################
+
+    
+
+
         # dealing with left-padding
         if attention_mask is not None:
             i = i.mul_(attention_mask.unsqueeze(-1))
-        i, f = map(lambda x: rearrange(x, 'b l (h d) -> b h l d', h=self.num_heads), (i, f))
 
-        recurrent_state = last_state[-1] if use_cache else None
+
+        B, T, D = i.shape
+        if self.recurrent_state is None:
+            self.recurrent_state = torch.zeros((B,T,D,D), dtype=torch.float32, device=i.device)
+        
+
         if mode == 'fused_recurrent':
-            o, recurrent_state = fused_recurrent_hgrn(i, f, initial_state=recurrent_state, output_final_state=use_cache)
+            for _ in range(T):
+
+                self.recurrent_state = torch.matmul(self.recurrent_state, torch.diag_embed(f)) + torch.einsum('...i,...j->...ij', i, (1 - f))
+                o = torch.matmul(self.recurrent_state, g.unsqueeze(-1)).squeeze(-1)
+
         else:
             raise NotImplementedError(f"Not supported mode `{mode}`.")
-
+        
+     
         if past_key_values is not None:
             if self.use_short_conv:
                 if self.share_conv_kernel:
-                    last_state = (conv_state, recurrent_state)
+                    last_state = (conv_state, self.recurrent_state)
                 else:
-                    last_state = (conv_state_i, conv_state_f, recurrent_state)
+                    last_state = (conv_state_i, conv_state_f, self.recurrent_state)
             else:
-                last_state = (recurrent_state,)
+                last_state = (self.recurrent_state,)
             past_key_values.update(last_state, self.layer_idx, i.shape[2])
-
-        o = self.g_norm(self.g_proj(hidden_states), rearrange(o, 'b h l d -> b l (h d)'))
+        
+        o = self.rms_norm_custom(o)
         o = self.o_proj(o)
 
         return o, None, past_key_values
